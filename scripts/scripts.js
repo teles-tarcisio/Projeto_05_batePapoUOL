0ab891ed0d86572f1481333f526db8f976d962e8
Tarcísio Teles <tfst@poli.br>
2021-08-04 22:20:39 -0300

    basic divs/colors ok, initiating server communications

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+const POST_LOGIN_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants";
+const POST_KEEPALIVE_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/status";
+const GET_MESSAGES_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages";
+const POST_MESSAGES_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages";
+
+function writeMessage() {
+    console.log("write here was clicked");
+}
