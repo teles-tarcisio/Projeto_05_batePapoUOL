faa85d68537e7ddd9b364a894592c9738a002a87
Tarcísio Teles <tfst@poli.br>
2021-08-04 23:33:11 -0300

    basic post (user login) and get (server messages) working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,6 +3,26 @@ const POST_KEEPALIVE_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/u
 const GET_MESSAGES_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages";
 const POST_MESSAGES_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages";
 
-function writeMessage() {
-    console.log("write here was clicked");
+function userCheckIn() {
+    const user = {
+        name: "Pixels_The_Dog"
+    };
+    const checkInPromise = axios.post(POST_LOGIN_URL, user);
+    console.log("checkin: ");
+    checkInPromise.then(console.log);
+}
+
+function getAllMessages() {
+    const getMessagesPromise = axios.get(GET_MESSAGES_URL);
+    console.log("getMessages: ");
+    getMessagesPromise.then(console.log);
+    getMessagesPromise.then(printMessages);
+}
+
+function printMessages(messages) {
+    //console.log("got messages");
+    let mainChat = document.querySelector(".main-chat");
+    for (i = 0; i < 100; i++) {
+        mainChat.innerHTML += `<li> ${messages.data[i].time} ${messages.data[i].from} ${messages.data[i].text} </li>`;
+    }
 }
